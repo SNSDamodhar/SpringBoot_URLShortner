be999be1ae8960146bdd01928e08e722cd051798
Damodhar Sadhu <sadhudamodhar12@gmail.com>
2022-01-26 13:01:05 +0530

    Removed unwanted import statements

--- src/main/java/com/url/shortner/exception/GlobalExceptionHandler.java
+++ src/main/java/com/url/shortner/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.url.shortner.exception;
 
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
--- src/main/java/com/url/shortner/model/UrlEntity.java
+++ src/main/java/com/url/shortner/model/UrlEntity.java
@@ -1,6 +1,5 @@
 package com.url.shortner.model;
 
-import java.time.LocalDate;
 import java.util.Date;
 
 import javax.persistence.CascadeType;
